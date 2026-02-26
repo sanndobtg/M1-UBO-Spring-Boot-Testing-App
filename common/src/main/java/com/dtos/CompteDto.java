9bf08f517dfececcd3a0c45b9c2407f43b9ebb7a
Sanndo Batengue <sanndobatengue@gmail.com>
2026-02-26 18:04:55 +0100

    chore : premiere version de l'architecture du projet fonctionnelle

---
+++ common/src/main/java/com/dtos/CompteDto.java
@@ -0,0 +1,38 @@
+package com.dtos;
+
+
+import com.enums.RoleCompte;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompteDto {
+
+
+    private Long Id;
+
+
+    private RoleCompte role;
+
+
+    private String pseudo;
+
+
+    private String nom;
+
+
+    private String prenom;
+
+
+    private String dateDeNaissance;
+
+
+    private String adresse;
+
+    private String mdp;
+}
